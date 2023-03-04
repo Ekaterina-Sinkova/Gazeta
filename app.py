2830ba0aef168d4b455d9851f17f39daf49d21be
Ekaterina Sinkova <ascold.89@mail.ru>
2023-03-05 00:08:25 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,30 @@
+import faiss
+import numpy as np
+import streamlit as st
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+from loader import load_model, load_data
+
+st.title('Поиск похожих статей Gazeta.ru')
+st.text('Система возвращает 10 результатов: краткие описания и ссылки на новости')
+
+#загружаем модель, данные и индекс
+embedder = load_model()
+df = load_data()
+vectors = np.stack(df['embedding'].values, axis=0)
+index = faiss.IndexFlatL2(vectors.shape[1])
+index.add(vectors)
+
+def get_top10(index, query):
+    """Возвращает 10 статей, который максимально похожи на запрос"""
+    query_embedding = embedder.encode(query, convert_to_tensor=False)
+    _, I = index.search(np.array(query_embedding).reshape((1,768)), 10)
+    result = df.iloc[I[0]][['summary', 'url']]
+    return result
+
+query = st.text_area("Введите запрос")
+if st.button("Искать"):
+    result = get_top10(index, query)
+    st.write('Результат:', result)
---
+++ loader.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+
+
+embedder = None
+df = None
+
+def load_model():
+    """Load the model"""
+    global embedder
+    if not embedder:
+        embedder = SentenceTransformer('paraphrase-multilingual-mpnet-base-v2')
+    return embedder
+
+def load_data():
+    """Load the data"""
+    global df
+    if not df:
+        df1 = pd.read_pickle(r'C:\Users\esinkova\Desktop\Lingv\Scripts\app\my_dataframe1.pkl')
+        df2 = pd.read_pickle(r'C:\Users\esinkova\Desktop\Lingv\Scripts\app\my_dataframe2.pkl')
+        df3 = pd.read_pickle(r'C:\Users\esinkova\Desktop\Lingv\Scripts\app\my_dataframe3.pkl')
+        return pd.concat([df1, df2, df3], axis=0)
